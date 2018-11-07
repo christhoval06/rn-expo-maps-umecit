7dffe3e44093b6c6e9455c6845baed49e68e775d
Christhoval Barba <christhoval06@gmail.com>
2018-11-07 09:11:09 -0500

    add routes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Router from './routes';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={[styles.text, styles.bold]}>Hola Mundo!!</Text>
-        <Text style={styles.text}>Expo</Text>
-
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'black',
-  },
-  bold:{
-    fontWeight: '900',
-    fontSize: 30,
-  }
-});
+export default () => <Router/>;
