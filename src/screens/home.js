54c2cf4ce49ab52531aa6e538d33ce94820ce43c
Christhoval Barba <christhoval06@gmail.com>
2018-11-07 09:24:42 -0500

    add buttons to screens

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import {Button, Text, View} from 'react-native';
 
 import styles from '../styles';
 
 export default class Home extends Component {
 	render() {
+		const {navigation} = this.props;
 		return (
 			<View style={styles.container}>
 				<Text style={[styles.text, styles.bold]}>Home</Text>
 				<Text style={styles.text}>Screen</Text>
 
+				<Button
+					onPress={() => navigation.navigate('/main')}
+					title="Go to Main"/>
 			</View>
 		);
 	}
--- src/screens/main.js
+++ src/screens/main.js
@@ -1,15 +1,20 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 
 import styles from '../styles';
 
 export default class Main extends Component {
 	render() {
+		const {navigation} = this.props;
 		return (
 			<View style={styles.container}>
 				<Text style={[styles.text, styles.bold]}>Main</Text>
 				<Text style={styles.text}>Screen</Text>
 
+
+				<Button
+					onPress={() => navigation.navigate('/home')}
+					title="Return to Home"/>
 			</View>
 		);
 	}
