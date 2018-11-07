81a32aadb5b7dcbec0171fc903ed2694db898cdf
Christhoval Barba <christhoval06@gmail.com>
2018-11-07 08:39:11 -0500

    iniciando app

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.text, styles.bold]}>Hola Mundo!!</Text>
+        <Text style={styles.text}>Expo</Text> 
+   
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+  },
+  bold:{
+    fontWeight: '900',
+    fontSize: 30,
+  }
+});
